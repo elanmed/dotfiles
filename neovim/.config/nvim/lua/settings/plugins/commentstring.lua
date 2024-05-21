e59147b509654cac39f0886c0e21f3fdb7023470
Elan Medoff <elanmedoff@Elans-MacBook-Pro.local>
2024-05-21 19:09:25 -0400

    rename neovim when adding submodule

--- neovim/.config/nvim/lua/settings/plugins/commentstring.lua
+++
@@ -1,7 +0,0 @@
-local commentstring = require('ts_context_commentstring')
-local h = require "shared.helpers"
-
-h.let.skip_ts_context_commentstring_module = true
-commentstring.setup {
-  enable_autocmd = false,
-}
