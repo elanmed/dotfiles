bf0a5dc3b9f99aa2674ded2f8b67aa9d21ef4d69
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-07-26 11:54:37 -0400

    update scripts to use nvim exec

--- scripts/.local/bin/n.sh
+++ scripts/.local/bin/n.sh
@@ -3,8 +3,8 @@ uuid=$(uuidgen)
 uuid=${uuid:0:3}
 tmux new-session -d -s "n-${uuid}"
 window=0
-tmux rename-window -t "n-${uuid}":0 'nvim'
-tmux send-keys 'nvim ' $1 C-m
+tmux rename-window -t "n-${uuid}":0 "$(which nvim)"
+tmux send-keys "$(which nvim) " $1 C-m
 tmux split-window -h
 tmux send-keys "cd $1 && clear" C-m
 tmux select-pane -L
--- scripts/.local/bin/ps.sh
+++ scripts/.local/bin/ps.sh
@@ -3,8 +3,8 @@ uuid=$(uuidgen)
 uuid=${uuid:0:8}
 tmux new-session -d -s "n-${uuid}"
 window=0
-tmux rename-window -t "n-${uuid}":0 'nvim'
-tmux send-keys "nvim $1" C-m
+tmux rename-window -t "n-${uuid}":0 "$(which nvim)"
+tmux send-keys "$(which nvim) $1" C-m
 window=1
 tmux new-window -t "n-${uuid}":1
 tmux select-window -t "n-${uuid}":1
