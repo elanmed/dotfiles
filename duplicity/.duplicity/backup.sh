25d2defb788972223517fa1bedc181391d7de3bc
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-09-21 23:03:00 -0400

    cleanup duplicity script

--- duplicity/.duplicity/backup.sh
+++ duplicity/.duplicity/backup.sh
@@ -1,16 +1,23 @@
-source "$HOME/.dotfiles/duplicity/.duplicity/.env"
+source ~/.dotfiles/duplicity/.duplicity/.env
+source ~/.dotfiles/helpers.sh
 
 LOG_FILE="$HOME/.dotfiles/duplicity/.duplicity/info.log"
 echo "" > $LOG_FILE
 
-# TODO: update dir
+REMOVE_START=`date +%s`
+duplicity remove-older-than "2M" "$DUPLICITY_DEST" --force --log-file "$LOG_FILE"
+REMOVE_END=`date +%s`
+REMOVE_RUNTIME=$((REMOVE_END-REMOVE_START))
+h_cecho --noop "remove took $REMOVE_RUNTIME seconds"
 
-duplicity \
-    --full-if-older-than "7D" \
-    --log-file "$LOG_FILE" \
-    "$HOME/Desktop/test-dir/" \
-    "boto3+s3://macbook-pro-2020/"
+BACKUP_START=`date +%s`
+duplicity --full-if-older-than "7D" "$DUPLICITY_SOURCE" "$DUPLICITY_DEST" --log-file "$LOG_FILE" 
+BACKUP_END=`date +%s`
+BACKUP_RUNTIME=$((BACKUP_END-BACKUP_START))
+h_cecho --noop "backup took $BACKUP_RUNTIME seconds"
 
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset PASSPHRASE
+unset DUPLICITY_SOURCE
+unset DUPLICITY_DEST
