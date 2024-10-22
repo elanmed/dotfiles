f3e0907ca17982256717c62ca8fd45cab5f810e9
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-21 22:45:29 -0700

    vi mode fzf widgets cleanup

--- zsh/.config/zsh/vi-mode.sh
+++ zsh/.config/zsh/vi-mode.sh
@@ -10,18 +10,11 @@ fi
 fzf_cmd_prefix='file="$(fzf)"; if [[ "$file" != "" ]]; then;'
 fzf_cmd_suffix=' "$file"; fi \n'
 
-# bindkey -sM vicmd '^F' "$fzf_cmd_prefix open $fzf_cmd_suffix"
-# bindkey -sM viins '^F' "$fzf_cmd_prefix nvim $fzf_cmd_suffix"
-#
-# bindkey -sM vicmd '^P' 'vi-insert; file_to_edit="$(fzf)"; if [[ "$file_to_edit" != "" ]]; then; nvim "$file_to_edit"; fi \n'
-# bindkey -sM viins '^P' 'vi-insert; file_to_edit="$(fzf)"; if [[ "$file_to_edit" != "" ]]; then; nvim "$file_to_edit"; fi \n'
-
-e_fzf() {
-  zle vi-insert
-  eval "file="$(fzf)"; if [[ "$file" != "" ]]; then; open "$file"; fi \n"
-}
-zle -N e_fzf
-bindkey -M vicmd '^F' e_fzf
+# TODO: issues using fzf with a widget registered with zle -N
+bindkey -sM vicmd '^P' "i $fzf_cmd_prefix nvim $fzf_cmd_suffix"
+bindkey -sM viins '^P' "$fzf_cmd_prefix nvim $fzf_cmd_suffix"
+bindkey -sM vicmd '^F' "i $fzf_cmd_prefix open $fzf_cmd_suffix"
+bindkey -sM viins '^F' "$fzf_cmd_prefix open $fzf_cmd_suffix"
 
 time_machine_backwards() {
   source ~/Desktop/cd_time_machine/main.sh --backwards
@@ -34,15 +27,20 @@ time_machine_forwards() {
 zle -N time_machine_backwards
 zle -N time_machine_forwards
 
-bindkey -M vicmd '^?' vi-backward-word
+w_exit() {
+  exit
+}
+zle -N w_exit
+
 bindkey -M viins '^E' autosuggest-execute
+bindkey -M vicmd '^?' vi-backward-word
 bindkey -M vicmd '^?' vi-backward-word
 bindkey -M vicmd '^O' time_machine_backwards
 bindkey -M viins '^O' time_machine_backwards
 bindkey -M vicmd '^I' time_machine_forwards
 bindkey -M viins '^I' time_machine_forwards
-bindkey -sM vicmd '^Y' 'exit\n'
-bindkey -sM viins '^Y' 'exit\n'
+bindkey -M vicmd '^Y' w_exit
+bindkey -M viins '^Y' w_exit
 bindkey -M vicmd '^G' clear-screen
 bindkey -M viins '^G' clear-screen
 bindkey -M viins '^S' expand-or-complete
