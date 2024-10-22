31ffb32b76f5f27c6f48e5726fa5c34d12d70423
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-21 23:22:39 -0700

    cleanup n and ps scripts

--- scripts/.local/bin/n.sh
+++ scripts/.local/bin/n.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 source ~/.dotfiles/helpers.sh
 
-if [[ "$TERM_PROGRAM" = tmux ]]
-then
-  tmux send-keys "nvim " "$1" "C-m"
+split_window() {
   tmux split-window -h
   tmux send-keys "cd $1 && clear" "C-m"
   tmux select-pane -L
   tmux resize-pane -Z
+}
+
+if [[ "$TERM_PROGRAM" = tmux ]]
+then
+  tmux send-keys "nvim " "$1" "C-m"
+  num_panes=$(tmux display-message -p '#{window_panes}')
+  [[ "$num_panes" == 1 ]] && split_window "$@"
   exit
 fi
+
 
 uuid=$(uuidgen)
 uuid=${uuid:0:3}
 tmux new-session -d -s "n-${uuid}"
 window=0
 tmux rename-window -t "n-${uuid}":0 "code"
-tmux send-keys "nvim " $1 C-m
-tmux split-window -h
-tmux send-keys "cd $1 && clear" C-m
-tmux select-pane -L
-tmux resize-pane -Z
+tmux send-keys "nvim " "$1" C-m
+split_window "$@"
 window=1
 tmux new-window -t "n-${uuid}":1
 tmux rename-window -t "n-${uuid}":1 "tests"
--- scripts/.local/bin/ps.sh
+++ scripts/.local/bin/ps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source ~/.dotfiles/helpers.sh
+
 if [[ "$TERM_PROGRAM" = "tmux" ]]
 then
   h_echo --error "only run ps.sh outside a tmux session!" 
@@ -15,9 +17,9 @@ window=1
 tmux new-window -t "n-${uuid}":1
 tmux select-window -t "n-${uuid}":1
 tmux send-keys "cd $1 && clear" C-m
-tmux send-keys 'open http://localhost:3001 && npm run dev' C-m
+tmux send-keys "open http://localhost:3001 && npm run dev" C-m
 tmux split-window -h
 tmux send-keys "cd $1 && clear" C-m
-tmux send-keys 'npx prisma studio' C-m
+tmux send-keys "npx prisma studio" C-m
 tmux select-window -t "n-${uuid}":0
 tmux attach-session -t "n-${uuid}"
