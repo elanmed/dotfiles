987a551bfef7de3c08d691c4bd2791f27e0d2ff6
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-26 21:38:27 -0700

    use dynamic nvm dir when installing nvm

--- nvm/bootstrap.sh
+++ nvm/bootstrap.sh
@@ -4,7 +4,9 @@ source ~/.dotfiles/helpers.sh
 h_validate_num_args --num=1 "$@"
 h_validate_package_manager "$1"
 
-if [[ -d ~/.nvm  ]]
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+
+if [[ -d "$NVM_DIR"  ]]
 then
   h_echo --mode=noop "nvm already installed"
 else
@@ -12,7 +14,7 @@ else
   PROFILE=/dev/null curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 
 h_echo --mode=doing "installing the latest version of node"
