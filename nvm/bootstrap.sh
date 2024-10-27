33d97efae640963a6ddc9c8f9da01b798ac90c0e
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-26 23:18:53 -0700

    issues with dynamic nvim_dir variable, using static install with git instead

--- nvm/bootstrap.sh
+++ nvm/bootstrap.sh
@@ -4,18 +4,17 @@ source ~/.dotfiles/helpers.sh
 h_validate_num_args --num=1 "$@"
 h_validate_package_manager "$1"
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-
+NVM_DIR="$HOME/.nvm"
 if [[ -d "$NVM_DIR"  ]]
 then
   h_echo --mode=noop "nvm already installed"
 else
   h_echo --mode=doing "installing nvm"
-  PROFILE=/dev/null curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 fi
 
-# load nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+h_echo --mode=doing "sourcing nvm"
+source "$NVM_DIR/nvm.sh" 
 
 h_echo --mode=doing "installing the latest version of node"
 nvm install 'lts/*'
