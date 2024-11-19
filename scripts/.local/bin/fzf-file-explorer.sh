9e3b21a0d8026ee337b0844e520b12d65c8e6ab4
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-11-18 22:40:30 -0500

    cleanup fzf explorer

--- scripts/.local/bin/fzf-file-explorer.sh
+++ scripts/.local/bin/fzf-file-explorer.sh
@@ -3,39 +3,38 @@
 temp_file=$(mktemp)
 echo 'open' > $temp_file
 
+base_header="
+CTRL-D to display directories | CTRL-F to display files
+CTRL-A to toggle all
+CTRL-O to open | CTRL-E to edit"
+
 # based on https://thevaluable.dev/practical-guide-fzf-example/
 selection=$(find -type d | fzf --multi --height=100% --border=sharp --layout=reverse-list \
---preview='tree -C {}' --preview-window='45%,border-sharp' \
+--preview="tree -C {}" --preview-window="45%,border-sharp" \
 --prompt='Dirs > ' \
 --bind="ctrl-o:execute-silent(eval 'echo open > "$temp_file"')" \
 --bind="ctrl-o:+change-header(
-CTRL-D to display directories | CTRL-F to display files
-CTRL-A to toggle all
-CTRL-O to open | CTRL-E to edit
-ENTER to OPEN a file / directory
+$base_header
+ENTER to OPEN
 )" \
 --bind="ctrl-e:execute-silent(eval 'echo edit > "$temp_file"')" \
 --bind="ctrl-e:+change-header(
-CTRL-D to display directories | CTRL-F to display files
-CTRL-A to toggle all
-CTRL-O to open | CTRL-E to edit
-ENTER to EDIT a file / directory
+$base_header
+ENTER to EDIT
 )" \
---bind='ctrl-d:change-prompt(Dirs > )' \
---bind='ctrl-d:+reload(find -type d)' \
---bind='ctrl-d:+change-preview(tree -C {})' \
---bind='ctrl-d:+refresh-preview' \
---bind='ctrl-f:change-prompt(Files > )' \
---bind='ctrl-f:+reload(find -type f)' \
---bind='ctrl-f:+change-preview(highlight -O ansi --force {})' \
---bind='ctrl-f:+refresh-preview' \
---bind='ctrl-a:toggle-all' \
---header '
-CTRL-D to display directories | CTRL-F to display files
-CTRL-A to toggle all
-CTRL-O to open | CTRL-E to edit
-ENTER to OPEN a file / directory
-'
+--bind="ctrl-d:change-prompt(Dirs > )" \
+--bind="ctrl-d:+reload(find -type d)" \
+--bind="ctrl-d:+change-preview(tree -C {})" \
+--bind="ctrl-d:+refresh-preview" \
+--bind="ctrl-f:change-prompt(Files > )" \
+--bind="ctrl-f:+reload(find -type f)" \
+--bind="ctrl-f:+change-preview(highlight -O ansi --force {})" \
+--bind="ctrl-f:+refresh-preview" \
+--bind="ctrl-a:toggle-all" \
+--header "
+$base_header
+ENTER to OPEN
+"
 )
 
 read -r action < "$temp_file"
