6c57be6d519c542a55dc05b63019d2ac58843d30
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-12-27 21:23:07 -0500

    move nvm into submodule

--- nvm/bootstrap.sh
+++ nvm/bootstrap.sh
@@ -24,16 +24,8 @@ done
 
 h_validate_package_manager "$package_manager"
 
-NVM_DIR="$HOME/.nvm"
-if [[ -d $NVM_DIR ]]; then
-  h_echo --mode=noop "nvm already installed"
-else
-  h_echo --mode=doing "installing nvm"
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-fi
-
 h_echo --mode=doing "sourcing nvm"
-source "$NVM_DIR/nvm.sh"
+source "$HOME/.nvm/nvm.sh"
 
 if $server_flag; then
   h_echo --mode=noop "SKIPPING: installing the latest version of node"
