520c33657492190cf16813bd2f74e92af2851842
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-12-27 22:07:26 -0500

    cleanup scripts dir

--- scripts/.local/bin/fzf-file-explorer.sh
+++ scripts/.local/bin/fzf-file-explorer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 temp_file=$(mktemp)
-echo 'open' > $temp_file
+echo open >"$temp_file"
 
 base_header="
 CTRL-D to display directories | CTRL-F to display files
@@ -9,45 +9,52 @@ CTRL-A to toggle all
 CTRL-O to open | CTRL-E to edit"
 
 # based on https://thevaluable.dev/practical-guide-fzf-example/
-selection=$(find -type d | fzf --multi --height=100% --border=sharp --layout=reverse-list \
---preview="tree -C {}" --preview-window="45%,border-sharp" \
---prompt='Dirs > ' \
---bind="ctrl-o:execute-silent(eval 'echo open > "$temp_file"')" \
---bind="ctrl-o:+change-header(
+selection=$(
+  find . -type d | fzf --multi --height=100% --border=sharp --layout=reverse-list \
+    --preview="tree -C {}" --preview-window="45%,border-sharp" \
+    --prompt='Dirs > ' \
+    --bind="ctrl-o:execute-silent(eval 'echo open > $temp_file')" \
+    --bind="ctrl-o:+change-header(
 $base_header
 ENTER to OPEN
 )" \
---bind="ctrl-e:execute-silent(eval 'echo edit > "$temp_file"')" \
---bind="ctrl-e:+change-header(
+    --bind="ctrl-e:execute-silent(eval 'echo edit > $temp_file')" \
+    --bind="ctrl-e:+change-header(
 $base_header
 ENTER to EDIT
 )" \
---bind="ctrl-d:change-prompt(Dirs > )" \
---bind="ctrl-d:+reload(find -type d)" \
---bind="ctrl-d:+change-preview(tree -C {})" \
---bind="ctrl-d:+refresh-preview" \
---bind="ctrl-f:change-prompt(Files > )" \
---bind="ctrl-f:+reload(find -type f)" \
---bind="ctrl-f:+change-preview(highlight -O ansi --force {})" \
---bind="ctrl-f:+refresh-preview" \
---bind="ctrl-a:toggle-all" \
---header "
+    --bind="ctrl-d:change-prompt(Dirs > )" \
+    --bind="ctrl-d:+reload(find -type d)" \
+    --bind="ctrl-d:+change-preview(tree -C {})" \
+    --bind="ctrl-d:+refresh-preview" \
+    --bind="ctrl-f:change-prompt(Files > )" \
+    --bind="ctrl-f:+reload(find -type f)" \
+    --bind="ctrl-f:+change-preview(highlight -O ansi --force {})" \
+    --bind="ctrl-f:+refresh-preview" \
+    --bind="ctrl-a:toggle-all" \
+    --header "
 $base_header
 ENTER to OPEN
 "
 )
 
-read -r action < "$temp_file"
-trap "rm -f $temp_file" 0 2 3 15
+read -r action <"$temp_file"
+trap 'rm -f $temp_file' 0 2 3 15
 
-if [[ "$selection" == "" ]]
-then 
+if [[ $selection == "" ]]; then
   return
 fi
 
-if [ -d "$selection" ]
-then
-  if [[ $action == open ]]; then open "$selection"; else cd "$selection"; fi
+if [ -d "$selection" ]; then
+  if [[ $action == open ]]; then
+    open "$selection"
+  else
+    cd "$selection" || exit
+  fi
 else
-  if [[ $action == open ]]; then open "$selection"; else nvim "$selection"; fi
+  if [[ $action == open ]]; then
+    open "$selection"
+  else
+    nvim "$selection"
+  fi
 fi
