ddc82af5b5e2f9894330d7d2e5b2f6fbb095cf4c
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-01-12 19:46:12 -0500

    more wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,27 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+local function is_linux()
+  return package.cpath:match("%p[\\|/]?%p(%a+)") == "so"
+end
+
+local function cmd_or_ctrl()
+  return is_linux() and 'CTRL' or 'CMD'
+end
+
 local config = wezterm.config_builder()
 
 config.font_size = 13.0
-
+config.font = wezterm.font(
+  'ComicCode Ligatures Nerd Font'
+)
 config.keys = {
-  { key = 'v', mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
-  { key = 't', mods = 'CTRL',       action = act.SpawnTab 'CurrentPaneDomain' },
-  { key = 'w', mods = 'CTRL',       action = act.CloseCurrentTab { confirm = false } },
-  { key = 'n', mods = 'CTRL',       action = act.SpawnWindow },
-  { key = '{', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
-  { key = '}', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'v', mods = cmd_or_ctrl(),             action = act.PasteFrom 'Clipboard' },
+  { key = 't', mods = cmd_or_ctrl(),             action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'w', mods = cmd_or_ctrl(),             action = act.CloseCurrentTab { confirm = false } },
+  { key = 'n', mods = cmd_or_ctrl(),             action = act.SpawnWindow },
+  { key = '{', mods = cmd_or_ctrl() .. '|SHIFT', action = act.ActivateTabRelative(-1) },
+  { key = '}', mods = cmd_or_ctrl() .. '|SHIFT', action = act.ActivateTabRelative(1) },
 }
 config.colors = {
   cursor_bg = '#b4b7b4',
