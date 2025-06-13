9593874c1e199a7f8819e641bd7f7d4e099dd5d0
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-13 00:27:17 -0400

    cleanup fzf-file-explorer script

--- scripts/.local/bin/fzf-file-explorer.sh
+++ scripts/.local/bin/fzf-file-explorer.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 fzf-file-explorer() {
-  local find_cmd="find $dir -name node_modules -prune -o -name .git -prune -o"
-  local show_preview_cmd
-  show_preview_cmd="[[ $(tput cols) -ge 100 ]] && echo '50%,border-sharp'  || echo 'hidden'"
-
-  local dir="$1"
+  dir="$1"
   if [[ $dir == "" ]]; then
     dir="."
   fi
 
-  temp_file=$(mktemp)
-  echo edit >"$temp_file"
+  find_base=(find "$dir" -name node_modules -prune -o -name .git -prune -o)
+  find_files_cmd="${find_base[*]} -type f -print"
+  find_dirs_cmd="${find_base[*]} -type d -print"
+
+  show_preview() {
+    if [[ $(tput cols) -ge 100 ]]; then
+      echo '50%,border-sharp'
+    else
+      echo 'hidden'
+    fi
+  }
+
+  temp_file="$(mktemp)"
+  echo "edit" >"$temp_file"
 
   base_header="
 CTRL-D to display directories | CTRL-F to display files
@@ -20,25 +28,29 @@ CTRL-O to open | CTRL-E to edit"
 
   # based on https://thevaluable.dev/practical-guide-fzf-example/
   selection=$(
-    eval "$find_cmd -type f -print" | fzf --multi --height=100% --border=sharp --layout=reverse-list \
-      --preview-window="$(eval "$show_preview_cmd")" \
-      --preview="highlight -O ansi --force {}" \
+    eval "$find_files_cmd" | fzf \
+      --multi \
+      --height=100% \
+      --border=sharp \
+      --layout=reverse-list \
+      --preview-window="$(show_preview)" \
+      --preview="bat --style=plain --color=always {}" \
       --prompt='Files > ' \
-      --bind="ctrl-o:execute-silent(eval 'echo open > $temp_file')" \
+      --bind="ctrl-o:execute-silent(echo \"open\" > $temp_file)" \
       --bind="ctrl-o:+change-header(
 $base_header
 <CR> to OPEN)" \
-      --bind="ctrl-e:execute-silent(eval 'echo edit > $temp_file')" \
+      --bind="ctrl-e:execute-silent(echo \"edit\" > $temp_file)" \
       --bind="ctrl-e:+change-header(
 $base_header
 <CR> to EDIT)" \
       --bind="ctrl-d:change-prompt(Dirs > )" \
-      --bind="ctrl-d:+reload(eval \"$find_cmd -type d -print\")" \
+      --bind="ctrl-d:+reload($find_dirs_cmd)" \
       --bind="ctrl-d:+change-preview(tree -C {})" \
       --bind="ctrl-d:+refresh-preview" \
       --bind="ctrl-f:change-prompt(Files > )" \
-      --bind="ctrl-f:+reload(eval \"$find_cmd -type f -print\")" \
-      --bind="ctrl-f:+change-preview(highlight -O ansi --force {})" \
+      --bind="ctrl-f:+reload($find_files_cmd)" \
+      --bind="ctrl-f:+change-preview(bat --style=plain --color=always {})" \
       --bind="ctrl-f:+refresh-preview" \
       --bind="ctrl-a:toggle-all" \
       --header "
@@ -47,7 +59,7 @@ $base_header
   )
 
   read -r action <"$temp_file"
-  trap 'rm -f $temp_file' 0 2 3 15
+  trap 'rm -f "$temp_file"' EXIT INT QUIT TERM
 
   if [[ $selection == "" ]]; then
     return
