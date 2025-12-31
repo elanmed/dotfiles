6eb71644a0e1487ea3297aea621d6f36bdd08a32
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-12-30 16:28:24 -0800

    update autosuggest highlight color

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -42,3 +42,5 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#969896"
