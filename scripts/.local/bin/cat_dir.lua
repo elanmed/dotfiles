5da099f054e71a5520ab8dbbf2fef6aae9f2becb
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-02-11 23:21:55 -0500

    remove prompt before printing

--- scripts/.local/bin/cat_dir.lua
+++ scripts/.local/bin/cat_dir.lua
@@ -78,11 +78,4 @@ end
 
 accum_string(abs_dir)
 
-local accumed_string = table.concat(accumed_files, "\n")
-io.write(("Accumulated content is %s lines. Print to stdout [Y/n]? "):format(#vim.split(accumed_string, "\n")))
-local confirm_input = io.read()
-if confirm_input == 'Y' then
-  io.write(accumed_string)
-else
-  io.write("Aborting\n")
-end
+io.write(table.concat(accumed_files, "\n"))
