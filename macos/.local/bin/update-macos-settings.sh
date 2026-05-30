2f15a8c0964a9bbea18eb545ff7bfeb8b60deedf
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-05-30 17:00:17 -0400

    add: update-macos-settings

---
+++ macos/.local/bin/update-macos-settings.sh
@@ -0,0 +1,6 @@
+defaults write com.apple.controlcenter "BatteryShowPercentage" -bool true
+defaults write com.apple.dock autohide -bool true
+defaults write NSGlobalDomain KeyRepeat -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
