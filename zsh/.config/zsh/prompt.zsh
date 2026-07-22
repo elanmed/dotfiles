f79b1e83b688892a53fff129addbfaaa83f53c0e
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-07-21 22:57:26 -0400

    perf: calculate prompt prefix once

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -4,34 +4,21 @@
 # in prompt strings each time the prompt is displayed
 setopt PROMPT_SUBST
 
-prompt_prefix() {
-  if h_is_podman; then
-    echo "%B%b"
-  elif h_is_macos; then
-    echo "􀂍 "
-  else
-    echo "􀀀 "
-  fi
-}
-
-prompt_dir_color() {
-  if h_is_podman; then
-    echo "green"
-  else
-    echo "yellow"
-  fi
-}
+if h_is_podman; then
+  _prompt_prefix="%B󰍇%b"
+  _prompt_dir_color="green"
+else
+  _prompt_prefix="􀀀 "
+  _prompt_dir_color="yellow"
+fi
 
 prompt_git_branch() {
   local branch
   branch=$(git symbolic-ref HEAD 2>/dev/null | cut -d '/' -f 3)
   if [[ -n $branch ]]; then
-    echo "on %F{magenta} $branch%f"
+    echo "on %F{magenta} $branch%f"
   fi
 }
-
-_prompt_prefix="$(prompt_prefix)"
-_prompt_dir_color="$(prompt_dir_color)"
 
 PROMPT='%B%F{'$_prompt_dir_color'}%~%f%b $(prompt_git_branch)
 '$_prompt_prefix' :: '
