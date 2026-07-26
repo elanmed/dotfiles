d0fb43d15589fcf3f0d86fc75406b70e38589824
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-07-25 21:13:21 -0400

    polish: prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -19,7 +19,7 @@ if h_is_podman; then
   _prompt_prefix="%B󰍇%b"
   _prompt_dir_color="green"
 else
-  _prompt_prefix='$(randicon)  '
+  _prompt_prefix='$(randicon)'
   _prompt_dir_color="yellow"
 fi
 
@@ -32,4 +32,4 @@ prompt_git_branch() {
 }
 
 PROMPT='%B%F{'$_prompt_dir_color'}%~%f%b $(prompt_git_branch)
-'$_prompt_prefix' :: '
+'$_prompt_prefix'   \$ '
